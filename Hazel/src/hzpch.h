c0562f2fc48a452c649ae341a1f89a98409e694d
Hank Ibell <hwibell@gmail.com>
2020-06-29 18:17:50 -0400

    Add GLM math library.

--- Hazel/src/hzpch.h
+++ Hazel/src/hzpch.h
@@ -13,8 +13,18 @@
 #include <unordered_map>
 #include <unordered_set>
 
+
 /**************************** LOGGING *******************************/
 #include "Hazel/Log.h"
+
+
+/******************************* MATH *******************************/
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/scalar_constants.hpp>
 
 /**************************** WINDOWS *******************************/
 #ifdef HZ_PLATFORM_WINDOWS
